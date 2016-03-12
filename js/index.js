ec9dc4684c4b7a9dadf47b7295326423c422dd1b
zhongluyang <zhongluyang1@163.com>
2016-03-12 23:26:27 +0800

    Site updated: 2016-03-12 23:26:27

--- js/index.js
+++ js/index.js
@@ -5,9 +5,25 @@ $(document).ready(function() {
 	li.on('mouseenter', function(){
 		var index = $(this).index();
 		console.log(index);
-		var pos = index*66+3
+		var pos = index*54+index*45
 		bot.css("left",pos+"px");
 	})
 	
-	
+	 $(".mycarousel").mycarousel({
+	               
+	                //循环展示
+	                loop : true, 
+	                //显示小圆点
+	                dots : true,
+	                //显示箭头
+	                arrow : true,
+	                //首图随机
+	                random : true, 
+	                //轮播间隔
+	                time_space : 3000, 
+	                //动画结束时的回调函数
+	                callback: function(){ //
+	                   
+	                }
+	            });
 })

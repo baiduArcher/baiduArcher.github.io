1d97545f5ecf8d4b133ec3c0b6a25e46059d7d60
zhongluyang <zhongluyang1@163.com>
2016-03-22 18:17:00 +0800

    Site updated: 2016-03-22 18:16:59

--- js/task-18/index.js
+++ js/task-18/index.js
@@ -1,91 +1,106 @@
-//添加item
-function drawItem(number, judge) {
-	var queueView = document.getElementById("queueView");
-	var viewItem = document.createElement("div");
-	viewItem.className = "view-item";
-	viewItem.setAttribute('data-id', "item");
-	viewItem.innerHTML = number;
-	if (judge === "last") {
-		queueView.appendChild(viewItem);
-	} else if (judge === "before") {
-		queueView.insertBefore(viewItem, queueView.firstChild);
-	}
-};
+var queueView = document.getElementById("queueView");
 
-//删除item
-function removeItem(judge) {
-	var queueView = document.getElementById("queueView");
-	if (judge === "last") {
-		if(queueView.lastChild){
-		var value = queueView.lastChild.innerHTML;
-		queueView.removeChild(queueView.lastChild);
-		alert(value);
-		} else{
-			alert("大兄弟！没啦！");
-		}
-
-	} else if (judge === "before") {
-		if(queueView.firstChild){
-		var value = queueView.firstChild.innerHTML;
-		queueView.removeChild(queueView.firstChild);
-		alert(value);
-		} else{
-			alert("大兄弟！没啦！");
-		}
-
-	} else if (judge === "item") {
-		queueView.removeChild(arguments[1]);
-	}
-};
-
-//判断字符串是否为数字,支持负数,浮点    
-function checkRate(inputValue) {
-	var check = /^-?[0-9]+.?[0-9]+$/;
-	if (!check.test(inputValue)) {
-		alert("请输入数字");
-		document.getElementById("input").value = "";
-		return false
-	}
-	return true
-};
-
-	//处理函数
 var actionList = {
-	"input": function() {	
-	},
 	//左侧入
 	"left-enqueue": function() {
 		var inputValue = document.getElementById("input").value;
 		if (checkRate(inputValue)) {
-			drawItem(inputValue, "before");
+			addItem["addBefore"](inputValue);
 		}
 	},
 	//右侧入
 	"right-enqueue": function() {
 		var inputValue = document.getElementById("input").value;
 		if (checkRate(inputValue)) {
-			drawItem(inputValue, "last");
+			addItem["addLast"](inputValue);
 		}
 	},
 	//左侧出
 	"left-dequeue": function() {
-		removeItem("before");
+		removeItem["removeBefore"]();
 	},
 	//右侧出
 	"right-dequeue": function() {
-		removeItem("last");
+		removeItem["removeLast"]();
 	},
 	//移除item
 	"item": function(target) {
-		removeItem("item", target);
+		removeItem["removeItem"](target);
+	}
+}
+
+var generateItem= function(number){
+		var viewItem = document.createElement("div");
+		viewItem.className = "view-item";
+		viewItem.setAttribute('data-id', "item");
+		viewItem.innerHTML = number;
+		return viewItem
+};
+
+var addItem ={
+	//向前插入元素
+	addBefore: function(number) {
+	queueView.insertBefore(generateItem(number), queueView.firstChild);
+	},
+	//向后插入元素
+	addLast : function(number){
+	queueView.appendChild(generateItem(number));
 	}
 };
-//click事件委托给body
-document.body.addEventListener("click", function(event) {
+
+var removeItem ={
+	//向前删除元素
+	removeBefore: function(){
+		if(queueView.lastChild){
+			var value = queueView.firstChild.innerHTML;
+			queueView.removeChild(queueView.firstChild);
+			alert(value);	
+		} else{
+			alert("没了");
+		}
+	},
+	//向后删除元素
+	removeLast: function(){
+		if(queueView.lastChild){
+		var value = queueView.lastChild.innerHTML;
+		queueView.removeChild(queueView.lastChild);
+		alert(value);
+		}else{
+			alert("没了");
+		};
+	},
+	removeItem: function(target){
+		queueView.removeChild(target);
+	}
+	
+}
+
+var checkRate = function (inputValue) {
+	var check = /^-?[0-9]+\.?[0-9]+$/;
+	if (!check.test(inputValue)) {
+		alert("请输入数字");
+		document.getElementById("input").value = "";
+		return false;
+	}
+	return true
+}
+
+var buttonContainer = document.getElementById("button-container");
+buttonContainer.addEventListener("click", function(event) {
+actionClick();
+}, false);	
+
+var itemsContainer = document.getElementById("queueView");
+itemsContainer.addEventListener("click", function(event) {
+actionClick();
+}, false);	
+
+//点击函数
+var actionClick = function(){
 	var target = event.target;
 	//分配处理函数
-	var actionName = target.getAttribute('data-id')
+	var actionName = target.getAttribute('data-id');
 	var action = actionList[actionName];
 	action(target);
 	document.getElementById("input").value = "";
-}, false);
+};
